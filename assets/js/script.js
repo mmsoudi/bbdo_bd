bb46003365b2c6bc0bd9750fedc4d1f2e1718e53
Gan Mahmud <gan.mahmud@gmail.com>
2015-08-20 02:51:44 +0600

    nav
    mobile nav
    desktop nav

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var appMaster = {
+
+	nav_mobile: function(){
+
+		$('.menuswitch').click(function(){
+			$('.menuswitch').toggleClass('hidden-nav');
+		});
+		$('.burger').click(function(){
+			$('.menuswitch').toggleClass('hidden-nav');
+		});
+		function domino(selector,add,dur){
+			selector = $(String(selector));
+			selector.each(function(){
+				var index = $(this).index();
+				var that = $(this);
+				dur = dur || 100;
+				index = index*dur;
+				setTimeout(function(){
+					if (add == 'add'){
+						that.addClass('shown');
+					} else {
+						that.removeClass('shown');
+					}
+				},index);
+			});
+		}
+		$('.burger').click(function(){
+			domino('#nav_slide ul li','add');
+		});
+		$('.menuswitch').click(function(){
+			domino('#nav_slide ul li','remove');
+		});
+
+	}
+
+}
+
+$(document).ready(function() {
+	appMaster.nav_mobile();
+});
